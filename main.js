cc3ecf39da74b15985bb72e6704ed34b7134e595
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2018-11-10 00:24:14 +0900

    to function

--- main.js
+++ main.js
@@ -2,19 +2,23 @@ const request = require('request');
 const yaml = require('js-yaml');
 const fs = require('fs');
 
-const config = yaml.safeLoad(fs.readFileSync('./config.yml', 'utf8'));
+function getChannelMessages() {
+  const config = yaml.safeLoad(fs.readFileSync('./config.yml', 'utf8'));
 
-const options = {
-  url: `https://slack.com/api/channels.history?token=${config.token}&channel=${config.channel}&count=1000`,
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded'
-  }
-};
+  const options = {
+    url: `https://slack.com/api/channels.history?token=${config.token}&channel=${config.channel}&count=1000`,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  };
 
-request(options, (error, response, body) => {
-  const res = JSON.parse(body);
-  const messages = res.messages;
-  messages.reverse().forEach((message) => {
-    console.log(message.text);
+  request(options, (error, response, body) => {
+    const res = JSON.parse(body);
+    const messages = res.messages;
+    messages.reverse().forEach((message) => {
+      console.log(message);
+    });
   });
-});
+}
+
+getChannelMessages();
