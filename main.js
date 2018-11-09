d5e219fd32dce44d613da20241109b1057dd11b6
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2018-11-09 23:50:53 +0900

    innitial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const https = require('request');
+
+console.log(process.env);
+/*
+const options = {
+  url: `https://slack.com/api/channels.history?token=${token}&channel=${channel}&count=1000`,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+};
+
+request(options, (error, response, body) => {
+
+});
+*/
